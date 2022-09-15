53fa34f1b4cde64d3fc664f8a66c56beb58975e6
Omikal <109170459+Omikal@users.noreply.github.com>
2022-09-15 22:21:28 +0300

    Delete src/HW4_basic directory

--- src/HW4_basic/FlyException.java
+++
@@ -1,7 +0,0 @@
-package HW4_basic;
-
-public class FlyException extends Exception {
-    public FlyException(String message) {
-        super(message);
-    }
-}
--- src/HW4_basic/Flyable.java
+++
@@ -1,5 +0,0 @@
-package HW4_basic;
-
-public interface Flyable {
-    void fly() throws FlyException;
-}
